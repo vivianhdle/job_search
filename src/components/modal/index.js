cebe48981462e8a2d3e68361f5cc0513aff6cd2d
Vivian Le <vivianhdle@gmail.com>
2019-04-17 15:44:20 -0700

    wrote add axios call for job card form

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 import './modal.scss';
 import AddForm from './add-form';
+import axios from 'axios';
 
 class Modal extends Component{
-    handleAdd(values){
+    async handleAdd(values){
+        const {title,company,progress,contact,note,link} = values
         console.log('Form Values',values);
+        const resp = await axios.get(`/api/add_tracker_item.php?title=${title}&company=${company}&progress=${progress}&contact=${contact}&note=${note}&link=${link}`);
+        console.log(resp)
     }
     render(){
         const {isOpen}=this.props
