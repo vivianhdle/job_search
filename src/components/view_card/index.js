7480807fa579c072b4c560e56c3aff1c824852a8
Kenneth Li <likenhp@gmail.com>
2019-04-19 22:30:11 -0700

    added basic routing testing dummy data testing ability to pull data imported header

--- src/components/view_card/index.js
+++ src/components/view_card/index.js
@@ -4,21 +4,34 @@ import Header from '../general/header';
 
 class ViewCard extends Component{
     state = {
-        respData: []
+        respData: [],
     }
 
     componentDidMount(){
         this.getRespData();
+        
     }
 
     async getRespData(){
         const resp = await axios.get('/api/data/get_tracker_item.json');
-        console.log(resp);
+        this.setState({
+            respData: resp.data.data
+        })
+        
     }
 
     render(){
+        const respInfo = this.state.respData; 
+        console.log(respInfo);
+
         return(
-            <div>Hello, world.</div>
+            <div>
+                <Header/>
+                <div>Hello, world.</div>
+            </div>
+            
         )
     }
 }
+
+export default ViewCard;
