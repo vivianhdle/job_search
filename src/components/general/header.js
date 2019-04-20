9de8f3efcb0175b62bc208e90676d8b6df153713
Vivian Le <vivianhdle@gmail.com>
2019-04-19 17:39:51 -0700

    changed header color

--- src/components/general/header.js
+++ src/components/general/header.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './header.scss';
 
 function Header(props){
+    const style= {
+        "background-color":props.color
+    }
     return(
-        <div className="row title-header center">
+        <div className="row title-header center" style={style}>
             <div className="col s10 offset-s1">
                 <span>{props.title}</span>
             </div>
