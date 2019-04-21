ef3e2ab23d53c14cd9d90fdf59b20a60bf5c9a71
Vivian Le <vivianhdle@gmail.com>
2019-04-21 11:00:46 -0700

    view details css

--- src/components/general/input.js
+++ src/components/general/input.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 
 export default props => {
-    const {input,type="text",label,id,col='s12', meta:{error,touched}}=props;
+    const {input,classes='',type="text",label,id,col='s12', currentValue,meta:{error,touched}}=props;
+    console.log('input props',props)
     return(
         <div className={`input-field col ${col}`}>
-            <input type={type} {...input} id={id} />
+            <input className={`input ${id} ${classes}`} type={type} {...input} id={id} value={currentValue}/>
             <label htmlFor={id}>{label}</label>
         </div>
     )
--- src/components/view_card/view_form_card.js
+++ src/components/view_card/view_form_card.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Input from '../general/input';
 import {Field,reduxForm,FormSection} from 'redux-form';
 import ContactForm from '../prospect/contact';
@@ -25,25 +25,21 @@ function ViewFormCard(props){
         //         <div className="right-align created view-main-text"><em>Added {formatTime(created)}</em></div>
         //     </div>
         // </div>
-        <form action="">
-            <div className="row">
-                <Field id="company" col="s10 offset-s1" currentValue={company} name="company" component={Input} />
-            </div>
-            <div className="row">
-                <Field id="title" col="s10 offset-s1" currentValue={title} name="title" component={Input}/>
-            </div>
-            <div className="row">
-                <div className="right-align created" col="s10 offset-s1" ><em>Added {formatTime(created)}</em></div>
-            </div>
-            <div className="row">
-                <Field id="link" col="s10 offset-s1" currentValue={link} name="link" component={Input}/>
-            </div>
-            <ContactList contact={contact}/>
-            <NoteList note={note}/>
-            <div className="btn-wrapper row">
-                <button className="btn blue-grey submit-button">Submit</button>
-            </div>
-        </form>
+        <Fragment>
+            <Header title={progress} bgcolor="rgba(243, 243, 243, 0.856)"/>
+            <form action="">
+                <div className="row">
+                    <Field id="company" col="s10 offset-s1" currentValue={company} name="company" component={Input} />
+                    <Field id="title" col="s10 offset-s1" currentValue={title} name="title" component={Input}/>
+                    <Field id="link" col="s10 offset-s1" currentValue={link} name="link" component={Input}/>
+                </div>
+                <div className="row">
+                    <div className="s10 right-align offset-s1 col created" col="s10 offset-s1" ><em>Added {formatTime(created)}</em></div>
+                </div>
+                <ContactList contact={contact}/>
+                <NoteList note={note}/>
+            </form>
+        </Fragment>  
     )
 }
 
