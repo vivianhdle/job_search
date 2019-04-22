2e6cea7e1663627fd48f9c7db984f11d848248be
Jun Giang <jungiang@yahoo.com>
2019-04-22 09:32:14 -0700

    contacts is now an array that can have multiple contact items

--- src/components/prospect/add-form.js
+++ src/components/prospect/add-form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { reduxForm, Field, FormSection } from 'redux-form';
 import Input from '../general/input';
@@ -7,34 +7,54 @@ import DropDown from './progress';
 import Header from '../general/header';
 import TextArea from '../general/textarea';
 
-const AddJobCardForm = props => {
-    const { add, handleSubmit } = props;
-    return (
-        <form onSubmit={handleSubmit(add)}>
-            <Header title="Add Job Prospect" alignment="left-align" margin="5%" bgcolor="white"/>
-            <DropDown col="s10 offset-s1"/>
-            <div className="row">
-                <Field id="title" col="s10 offset-s1" name="title" component={Input} label="Job Title" />
-            </div>
-            <div className="row">
-                <Field id="company" col="s10 offset-s1" name="company" component={Input} label="Company Name" />
-            </div>
-            <div className="row">
-                <Field id="link" col="s10 offset-s1" name="link" component={Input} label="Posting Link" />
-            </div>
-            <div className="row">
-                <FormSection name='contact' col="s10 offset-s1">
-                    <ContactForm />
-                </FormSection>
-            </div>
-            <div className="row">
-                <Field id="note" col="s10 offset-s1" name="note" component={TextArea} label="Notes" />
-            </div>
-            <div className="btn-wrapper row">
-                <button className="btn blue-grey submit-button">Submit</button>
-            </div>
-        </form>
-    )
+class AddJobCardForm extends Component {
+    state = {
+        contactForm: [],
+        contactCount: 0
+    }
+    componentDidMount(){
+        this.addNewContact();
+    }
+    addNewContact = () => {
+        let {contactForm, contactCount} = this.state;
+        contactForm = [...contactForm, <ContactForm key={contactCount}/>]
+        contactCount++;
+        this.setState({
+            contactForm,
+            contactCount
+        })
+    }
+    render() {
+        const { add, handleSubmit } = this.props;
+        const {contactForm} = this.state;
+        console.log(contactForm);
+        return (
+            <form onSubmit={handleSubmit(add)}>
+                <Header title="Add Job Prospect" alignment="left-align" margin="5%" bgcolor="white" />
+                <DropDown col="s10 offset-s1" />
+                <div className="row">
+                    <Field id="title" col="s10 offset-s1" name="title" component={Input} label="Job Title" />
+                </div>
+                <div className="row">
+                    <Field id="company" col="s10 offset-s1" name="company" component={Input} label="Company Name" />
+                </div>
+                <div className="row">
+                    <Field id="link" col="s10 offset-s1" name="link" component={Input} label="Posting Link" />
+                </div>
+                <div className="row">
+                    {contactForm}
+                    <button className="btn" onClick={this.addNewContact}>add</button>
+                </div>
+                <div className="row">
+                    <Field id="note" col="s10 offset-s1" name="note" component={TextArea} label="Notes" />
+                </div>
+                <div className="btn-wrapper row">
+                    <button className="btn blue-grey submit-button">Submit</button>
+                </div>
+            </form>
+        )
+
+    }
 }
 
 // function validate(title, company){
--- src/components/prospect/contact.js
+++ src/components/prospect/contact.js
@@ -1,32 +1,35 @@
-import React, {Component, Fragment} from 'react';
-import {Field} from 'redux-form';
+import React, { Component, Fragment } from 'react';
+import { Field, FormSection } from 'redux-form';
 import Input from '../general/input';
 
-class ContactForm extends Component{
-    componentDidMount(){
+class ContactForm extends Component {
+    componentDidMount() {
         M.Collapsible.init(this.contact);
     }
 
-    render(){
-        return(
-            
+    render() {
+        return (
+
             <Fragment>
-            <ul className="collapsible" ref={(element) => {
-                this.contact = element
-            }}>
-                <li>
-                    <div className="collapsible-header"><i className="material-icons">contacts</i>Contacts</div>
-                    <div className="collapsible-body">
-                        <Field id="name" col="s12" name="name" component={Input} label="Name"/>
-                        <Field id="email" col="s12" name="email" component={Input} label="Email"/>
-                        <Field id="phone" col="s12" name="phone" component={Input} label="Phone"/>
-                    </div>
-                </li>
-            </ul>
+                <FormSection name='contact' col="s10 offset-s1">
+                    <ul className="collapsible" ref={(element) => {
+                        this.contact = element
+                    }}>
+                        <li>
+                            <div className="collapsible-header"><i className="material-icons">contacts</i>Contacts</div>
+                            <div className="collapsible-body">
+                                <Field id="name" col="s12" name="name" component={Input} label="Name" />
+                                <Field id="email" col="s12" name="email" component={Input} label="Email" />
+                                <Field id="phone" col="s12" name="phone" component={Input} label="Phone" />
+                            </div>
+                        </li>
+                    </ul>
+                </FormSection>
+
             </Fragment>
         );
     }
-    
+
 }
 
 export default ContactForm;
