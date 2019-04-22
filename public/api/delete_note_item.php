6cd1ff2d93e4bf59d642519a2955d8f46c4dc9f2
jungiang <jungiang@yahoo.com>
2019-04-22 14:48:55 -0700

    added delete note item endpoint and postman request data

---
+++ public/api/delete_note_item.php
@@ -0,0 +1,43 @@
+<?php
+require_once('functions.php');
+set_exception_handler('handleError');
+require_once('config.php');
+require_once('mysqlconnect.php');
+
+$json_input = file_get_contents("php://input");
+$input = json_decode($json_input, true);
+
+// if(!empty($_GET['user_id'])){
+//     throw new Exception('You must send a user_id with your request');
+// }
+
+if(empty($input['tracker_id'])){
+    throw new Exception('You must send a tracker id with your request');
+}
+
+if(empty($input['note_id'])){
+    throw new Exception('You must send a note id with your request');
+}
+
+$output['success'] = false;
+
+$user_id = 1;
+$tracker_id = $input['tracker_id'];
+$note_id = $input['note_id'];
+
+$note_item_query = "DELETE FROM `note_item` WHERE 
+    `tracker_id`=? AND `id`=?
+";
+
+$note_item_statement = mysqli_prepare($conn, $note_item_query);
+mysqli_stmt_bind_param($note_item_statement, 'is', $tracker_id, $note_id);
+$note_item_result = mysqli_stmt_execute($note_item_statement);
+$note_item_result = mysqli_stmt_get_result($note_item_statement);
+
+if(mysqli_affected_rows($conn) === 0){
+    throw new Exception('note was not deleted');
+}
+
+$output['success'] = true;
+print(json_encode($output));
+?>
