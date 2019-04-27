a6a3ad0e82660d286cbdd39c10ea93af0f2e5bfa
Vivian Le <vivianhdle@gmail.com>
2019-04-27 14:40:51 -0700

    built out both signin/signup page skeleton

--- src/components/account/sign_up/index.js
+++ src/components/account/sign_up/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
+import SignUpForm from './sign_up';
+import Header from '../../general/header';
+import './sign_up.scss'
 
 
-function SignUp(){
-    return(
-        <div> this is sign up page</div>
+class SignUp extends Component{
+    signUp=values=>{
+        console.log(values);
+    }
 
-    )
+    render(){
+        return(
+        <div className="signup-container row">
+            <div className="signup-box col m6 offset-m3 s10 offset-s1">
+                <div className="signup-content">
+                    <div className="photo"></div>
+                </div>
+                <Header alignment = "left-align" title="Sign Up" newClass = "teal-text text-darken-1"/>
+                <SignUpForm signUp={this.signUp}/>
+            </div>
+        </div>
+        )
+    }
+    
 }
 
 
