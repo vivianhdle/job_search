7e0dc3df6624d10ed1452284485c4986ffe1c720
Kenneth Li <likenhp@gmail.com>
2019-05-01 15:52:30 -0700

    fixed padding issue in view contacts no more white space when there is no content inside the contact collapse body

--- src/components/general/error_handler.js
+++ src/components/general/error_handler.js
@@ -4,14 +4,13 @@ import './error_handler.scss';
 
 class ErrorHandler extends Component {
     render() {
-        const { errorMsg, closeError, redirect } = this.props
+        const { errorMsg, closeError } = this.props
         return (
             <Modal modalClass="error-modal" mscss="error-wrapper">
                 <div>
                     <button className="exit" onClick={closeError}><i className="material-icons">close</i></button>
                 </div>
                 <div className="error-message-content center" >{errorMsg}</div>
-                {redirect && <button className="btn blue-grey" onClick={redirect}>Sign Up</button>}
             </Modal>
         )
     }
