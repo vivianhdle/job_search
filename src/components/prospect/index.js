9222cd5b9eda62f34a2f010273d6841e773e94f6
Vivian Le <vivianhdle@gmail.com>
2019-05-02 01:41:23 -0700

    took out debugger in code

--- src/components/prospect/index.js
+++ src/components/prospect/index.js
@@ -31,7 +31,6 @@ class Prospect extends Component {
                 link;
             }
             }
-            debugger;
             values = {
                 progress,
                 company,
