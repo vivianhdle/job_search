40187ccfbe311bb011d1710c83b7398df7004eba
Kenneth Li <likenhp@gmail.com>
2019-05-10 12:59:52 -0700

    changed the min char calue for the math.ceil to 25

--- src/components/general/textarea.js
+++ src/components/general/textarea.js
@@ -5,13 +5,13 @@ class TextArea extends Component {
     render() {
         const { label, id, col = 's12', input, icon } = this.props;
         const {value} = input;
-        const noteLength = Math.ceil(value.length/40);
+        const noteLength = Math.ceil(value.length/25);
         let notePx = noteLength*22;
         return (
             <div className="row">
                 <div className={`input-field col ${col}`}>
                     {icon && <i className="material-icons prefix">{icon}</i>}
-                    <textarea id={id} className="materialize-textarea GG" {...input} style={{height:`${notePx}px`}} ></textarea>
+                    <textarea id={id} className="materialize-textarea" {...input} style={{height:`${notePx}px`}} ></textarea>
                     <label htmlFor={id}>{!value && label}</label>
                 </div>
             </div>
