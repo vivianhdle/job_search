6f8de456069a41d58d27d6401812d03350f61a8d
Kenneth Li <likenhp@gmail.com>
2019-05-10 12:33:12 -0700

    added inline styling to textarea in roder to all ow user to see all text entered in notes

--- src/components/general/textarea.js
+++ src/components/general/textarea.js
@@ -5,11 +5,13 @@ class TextArea extends Component {
     render() {
         const { label, id, col = 's12', input, icon } = this.props;
         const {value} = input;
+        const noteLength = Math.ceil(value.length/40);
+        let notePx = noteLength*22;
         return (
             <div className="row">
                 <div className={`input-field col ${col}`}>
                     {icon && <i className="material-icons prefix">{icon}</i>}
-                    <textarea id={id} className="materialize-textarea" {...input}></textarea>
+                    <textarea id={id} className="materialize-textarea GG" {...input} style={{height:`${notePx}px`}} ></textarea>
                     <label htmlFor={id}>{!value && label}</label>
                 </div>
             </div>
